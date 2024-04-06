1eb53d056dd5d46f1dd20f7374c28d8a0c51cefa
jromog11 <jromog@unal.edu.co>
2024-04-06 11:45:58 -0500

    update

--- src/6b_api_server/autograding.py
+++ src/6b_api_server/autograding.py
@@ -4,7 +4,7 @@ import os
 
 # test code files
 assert os.path.exists("src/6b_api_client/config.json")
-assert os.path.exists("src/6b_api_client/app.py")
+assert os.path.exists("src/6b_api_client/server.py")
 
 
 # test run
